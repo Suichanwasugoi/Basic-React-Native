d54c0042e500eb5be488ac9e8c60b9c3345d5799
Suichanwasugoi <148204118+Suichanwasugoi@users.noreply.github.com>
2025-03-21 14:15:11 +0800

    Home Design & Routing

--- HelloWorld/app/(tabs)/_layout.tsx
+++ HelloWorld/app/(tabs)/_layout.tsx
@@ -30,7 +30,26 @@ const TabIcon =({ focused, icon, title }: any) => {
 
 const _layout = () => {
   return (
-    <Tabs screenOptions={{ tabBarShowLabel: false }}>
+    <Tabs screenOptions={{ 
+                    tabBarShowLabel: false,
+                    tabBarItemStyle:{
+                        width: '100%',
+                        height: '100%',
+                        justifyContent: 'center',
+                        alignItems: 'center',
+                    },
+                    tabBarStyle:{
+                        backgroundColor: '#0f0d23',
+                        borderRadius: 50,
+                        marginHorizontal: 20,
+                        marginBottom: 36,
+                        height: 52,
+                        position: 'absolute',
+                        overflow: 'hidden',
+                        borderWidth: 1,
+                        borderColor: '#0f0d23',
+                    },
+                    }}>
         <Tabs.Screen name="index"
                     options={{
                         title: 'Home',
